baed34cac9bc9bd10bb3739d88d8bf021affb403
mosheurpau <mosheur.pau@gamil.com>
2022-03-22 22:11:26 +0600

    added Product Component, component layout, display data

---
+++ src/components/Product/Product.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Product.css'
+const Product = (props) => {
+    // console.log(props.product);
+    const {name, img, seller, price, ratings} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt=""></img>
+            <p className='product-name'>{name}</p>
+        </div>
+    );
+};
+
+export default Product;
