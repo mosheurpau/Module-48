50117e86999d5c41139ee86397f10fe04ff1e679
mosheurpau <mosheur.pau@gamil.com>
2022-04-11 16:34:12 +0600

    Style sign up component and access email, password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Login.css";
+import google from "../../images/google-icon.png";
 
 const Login = () => {
   return (
@@ -10,20 +11,29 @@ const Login = () => {
         <form action="">
           <div className="input-group">
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="" />
+            <input type="email" name="email" id="" required />
           </div>
           <div className="input-group">
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="" />
+            <input type="password" name="password" id="" required />
           </div>
           <input className="form-submit" type="form-submit" value="Login" />
         </form>
-        <p>
+        <p className="form-link-p">
           New to Ema-John?
           <Link className="form-link" to="/signup">
             Create an account
           </Link>
         </p>
+        <div className="line-group">
+          <div></div>
+          <p>Or</p>
+          <div></div>
+        </div>
+        <button className="google-login-btn">
+          <img className="google-logo" src={google} alt="" /> Continue with
+          Google
+        </button>
       </div>
     </div>
   );
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,9 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import google from "../../images/google-icon.png";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleEmailBlur = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordBlur = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleCreateUser = (event) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError("Your two passwords did not match");
+      return;
+    }
+  };
+
   return (
-    <div>
-      <h2>Sing Up</h2>
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Sing Up</h2>
+        <form onSubmit={handleCreateUser}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input
+              onBlur={handleConfirmPasswordBlur}
+              type="password"
+              name="confirm-password"
+              id=""
+              required
+            />
+          </div>
+          <p style={{ color: "red" }}>{error}</p>
+          <input className="form-submit" type="form-submit" value="Sing Up" />
+        </form>
+        <p className="form-link-p">
+          Already have an account?
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
+        </p>
+        <div className="line-group">
+          <div></div>
+          <p>Or</p>
+          <div></div>
+        </div>
+        <button className="google-login-btn">
+          <img className="google-logo" src={google} alt="" /> Continue with
+          Google
+        </button>
+      </div>
     </div>
   );
 };
